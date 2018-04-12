dc8ee1f9fb28e04652645adb6560c75771805320
albernet <andrea042280@aol.com>
2018-04-12 14:44:55 -0400

    DW notes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,14 @@
 
 $("body").prepend('<div class=text-white style=position:fixed;top:0;bottom:0;width:100%;z-index:99999999;padding-top:12%;background-color:rgba(87,87,87,.95);" id="cover-page"><div id="cover-page-con" class="container text-center"style="opacity:100%;"><img alt="Andrea Bernet"class="img-fluid rounded-circle"src=http://via.placeholder.com/150x150><h1>Andrea Bernet</h1><p class=lead>...inspired innovative technology leader & follower.<p class=lead style=font-size:80%><i class="fa-1x fa-cog fa-spin fas"></i> we are preparing your experiance now...</div></div>');
 
-var parentPageID = 142;
+var parentPageID = 115;
 
 window.onload = getContent();
 
 function getContent() {
     $.ajax({
         method: 'GET',
-        url: 'https://me.andilyn80.com/wp-json/wp/v2/pages/' + parentPageID + '&order=asc&orderby=menu_order',
+        url: 'https://me.andilyn80.com/wp-json/wp/v2/pages/?parent=' + parentPageID + '&order=asc&orderby=menu_order',
         dataType: 'json',
         success: function (data) {
             if (data.length > 0) {
